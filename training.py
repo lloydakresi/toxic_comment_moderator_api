33480f6c60755d794850591e76a7ed4ce51181e0
lloydakresi <lloydakresi03@gmail.com>
2026-06-16 05:35:38 +0000

    split training set into class-imbalance-aware training and val sets

---
+++ training.py
@@ -0,0 +1,14 @@
+import polars as pl
+from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
+import numpy as np
+from eda import training_set, class_cols
+
+y = training_set.select(class_cols).to_numpy()
+X_dummy = np.zeros((training_set.height, 1))
+
+msss = MultilabelStratifiedShuffleSplit(
+    n_splits=1, test_size=0.1, random_state=42
+)
+train_idx, val_idx = next(msss.split(X_dummy, y))
+train_set = training_set[train_idx]
+val_set = training_set[val_idx]
